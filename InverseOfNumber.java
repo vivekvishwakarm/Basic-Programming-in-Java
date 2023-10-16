237534c7922d6bbb38a3e38c1a5c87b7aa957a4c
Vivek Vishwakarma <vivek6306619@gmail.com>
2023-10-16 11:01:25 +0530

    I solve a inverse of a number Qs which is learnt from there more

--- InverseOfNumber.java
+++ InverseOfNumber.java
@@ -2,9 +2,24 @@ import java.util.Scanner;
 
 public class InverseOfNumber {
     public static void main(String[] args) {
-        // Find out of a number
+        // Find inverse of a number like 21453 -> 23154
         Scanner sca = new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.print("Enter a number: ");
         int num = sca.nextInt();
+        int inv = 0;
+        int op = 1;
+        while(num != 0)
+        {
+            int od = num % 10;
+            int id = op;
+            int ip = od;
+
+            // make change to inv using ip and id
+            inv = inv + id * (int)Math.pow(10,ip-1);
+
+            num = num / 10;
+            op++;
+        }
+        System.out.println("Inverted Number is: "+inv);
     }
 }
